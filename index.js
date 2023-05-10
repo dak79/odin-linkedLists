f746be04bcd845c7f6be30e16f1dac0310aacceb
Daniele Campari <danielecampari@gmail.com>
2023-05-10 12:50:36 +0200

    add methods pop, shift and at

--- index.js
+++ index.js
@@ -55,10 +55,73 @@ const LinkedList = () => {
     size++
   }
 
+  /**
+   * removeOneNode.
+   * Remove node in a size 1 linked list
+   */
+  const removeOneNode = () => {
+    head = null
+    tail = null
+    size--
+  }
+
+  /**
+   * pop.
+   * Removes the last element from the list
+   *
+   * Time complexity: O(n)
+   */
+  const pop = () => {
+    if (isEmpty()) return
+
+    if (size === 1) return removeOneNode()
+
+    let prev = head
+
+    while (prev.next && prev.next !== tail) {
+      prev = prev.next
+    }
+
+    prev.next = null
+    tail = prev
+    size--
+  }
+
+  /**
+   * shift.
+   * Remove the first element from the list
+   *
+   * Time complexity: O(1)
+   */
+  const shift = () => {
+    if (isEmpty()) return
+
+    if (size === 1) return removeOneNode()
+
+    head = head.next
+    size--
+  }
+
+  /**
+   * at.
+   * Return the node value at given index
+   * @param {Number} index
+   *
+   * Time complexity: O(n)
+   */
+  const at = (index) => {
+    if (isEmpty()) return 'List is empty'
+
+    if (index < 0 || index > size || typeof index !== Number)
+      return 'Invalid index'
+
+    for (let i = 1, curr = head; i <= size; i++, curr = curr.next)
+      if (i === index) return curr.value
+
+    return 'Nothing found at that index'
+  }
+
   /** TODO:
-   * pop
-   * shift
-   * at(index)
    * contains(value)
    * find(value)
    * insertAt(value, index)
@@ -121,6 +184,9 @@ const LinkedList = () => {
   return {
     append,
     prepend,
+    pop,
+    shift,
+    at,
     isEmpty,
     getSize,
     getHead,
@@ -138,6 +204,26 @@ console.log(
     ll.getSize() === 1 ? 'is 1 node.' : `are ${ll.getSize()} nodes.`
   }`
 )
+console.log('---------------------------------')
+console.log('POP THE LAST ELEMENT - EMPTY LIST')
+ll.pop()
+console.log(ll.toString())
+console.log('---------------------------------')
+console.log('SHIFT THE FIRST ELEMENT - EMPTY LIST')
+ll.shift()
+console.log(ll.toString())
+console.log('---------------------------------')
+console.log('POP THE LAST ELEMENT - LIST SIZE 1')
+ll.append(12000)
+console.log(ll.toString())
+ll.pop()
+console.log(ll.toString())
+console.log('---------------------------------')
+console.log('SHIFT THE FIRST ELEMENT - LIST SIZE 1')
+ll.append(12000)
+console.log(ll.toString())
+ll.shift()
+console.log(ll.toString())
 console.log('---------------------------------')
 console.log('APPEND AT THE END')
 ll.append(40)
@@ -175,4 +261,32 @@ console.log(
 )
 console.log(ll.toString())
 console.log(`Linked list is empty? ${ll.isEmpty()}.`)
+console.log('---------------------------------')
+console.log('RETURN THE NODE AT GIVEN INDEX')
+console.log(`At index 1 there is value ${ll.at(1)}`)
+console.log(`At index 2 there is value ${ll.at(2)}`)
+console.log(`At index 3 there is value ${ll.at(3)}`)
+console.log(`At index 4 there is value ${ll.at(4)}`)
+console.log(`At index 5 there is value ${ll.at(5)}`)
+console.log(`At index 6 there is value ${ll.at(6)}`)
+console.log(ll.toString())
+console.log(`Index -9: ${ll.at(-9)}`)
+console.log(`Index 36: ${ll.at(36)}`)
+console.log(`Index A: ${ll.at('A')}`)
+console.log('---------------------------------')
+console.log('POP THE LAST ELEMENT')
+ll.pop()
+console.log(`The tail value is ${ll.getTail()}`)
+console.log(ll.toString())
+ll.pop()
+console.log(`The tail value is ${ll.getTail()}`)
+console.log(ll.toString())
+console.log('---------------------------------')
+console.log('SHIFT THE FIRST ELEMENT')
+ll.shift()
+console.log(`The head value is ${ll.getHead()}`)
+console.log(ll.toString())
+ll.shift()
+console.log(`The head value is ${ll.getHead()}`)
+console.log(ll.toString())
 console.log('---------------------------------')
